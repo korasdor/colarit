c6036c2f73ea9dd198d47aa04fb75b0d93ee0a1a
koras_dor <koras_dor@yahoo.com>
2017-08-27 03:52:41 +0500

    get access token from server environment

--- model/models.go
+++ model/models.go
@@ -1,5 +1,7 @@
 package model
 
+import "os"
+
 var (
 	DbSuccess       string
 	SerialKeyLength int = 10
@@ -8,5 +10,8 @@ var (
 const (
 	DB_SOURCE_NAME string = "bacdeca17611c1:a9e99d39@tcp(166.62.10.138:3306)/colar_db"
 	//DB_SOURCE_NAME string = "bacdeca17611c1:a9e99d39@tcp(eu-cdbr-west-01.cleardb.com:3306)/heroku_a4f8eaa86b016fe"
-	ACCESS_TOKEN string = "a1f93a52740422f24150e586cd43455101273dc4"
+)
+
+var (
+	ACCESS_TOKEN string = os.Getenv("ACCESS_TOKEN")
 )
