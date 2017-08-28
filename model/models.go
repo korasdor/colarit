2ccf5aba115222c2c50d8b9bdeadc5a7f54bef57
koras_dor <koras_dor@yahoo.com>
2017-08-28 05:37:10 +0500

    get data source name from environment

--- model/models.go
+++ model/models.go
@@ -5,13 +5,8 @@ import "os"
 var (
 	DbSuccess       string
 	SerialKeyLength int = 10
-)
 
-const (
-	DB_SOURCE_NAME string = "bacdeca17611c1:a9e99d39@tcp(166.62.10.138:3306)/colar_db"
 	//DB_SOURCE_NAME string = "bacdeca17611c1:a9e99d39@tcp(eu-cdbr-west-01.cleardb.com:3306)/heroku_a4f8eaa86b016fe"
-)
-
-var (
-	ACCESS_TOKEN string = os.Getenv("ACCESS_TOKEN")
+	DB_SOURCE_NAME string = os.Getenv("MYSQ_DATA_SOURCE_NAME")
+	ACCESS_TOKEN   string = os.Getenv("ACCESS_TOKEN")
 )
