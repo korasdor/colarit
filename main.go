541356d1a8815aad714c19ee4e7d35fff23ca4bb
koras_dor <koras_dor@yahoo.com>
2017-11-08 18:49:27 +0500

    set timeout to server response

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/korasdor/colarit/services"
 	"os"
 	"github.com/korasdor/colarit/utils"
+	"time"
 )
 
 func main() {
@@ -28,29 +29,31 @@ func main() {
 		utils.PrintOutput("Update book error")
 	}
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", handler.IndexHandler)
-	r.HandleFunc("/db_state", handler.DBStateHandler)
-	r.HandleFunc("/books", handler.BooksHandler)
-	r.HandleFunc("/update_books_template", handler.UpdateBooksHandler)
+	muxRouter := mux.NewRouter()
+	muxRouter.HandleFunc("/", handler.IndexHandler)
+	muxRouter.HandleFunc("/db_state", handler.DBStateHandler)
+	muxRouter.HandleFunc("/books", handler.BooksHandler)
+	muxRouter.HandleFunc("/update_books_template", handler.UpdateBooksHandler)
 
-	r.HandleFunc("/create_table/{table_name}/{access_token}", handler.CreateTableHandler)
-	r.HandleFunc("/fill_table/{table_name}/{serials_count}/{range_id}/{dealer_id}/{access_token}", handler.FillTableHandler)
-	r.HandleFunc("/fill_table_with_file/{table_name}/{file_name}/{range_id}/{dealer_id}/{access_token}", handler.FillTableWithFileHandler)
-	r.HandleFunc("/get_serial/{table_name}/{range_id}/{dealer_id}/{serials_format}/{access_token}", handler.GetSerialsHandler)
+	muxRouter.HandleFunc("/create_table/{table_name}/{access_token}", handler.CreateTableHandler)
+	muxRouter.HandleFunc("/fill_table/{table_name}/{serials_count}/{range_id}/{dealer_id}/{access_token}", handler.FillTableHandler)
+	muxRouter.HandleFunc("/fill_table_with_file/{table_name}/{file_name}/{range_id}/{dealer_id}/{access_token}", handler.FillTableWithFileHandler)
+	muxRouter.HandleFunc("/get_serial/{table_name}/{range_id}/{dealer_id}/{serials_format}/{access_token}", handler.GetSerialsHandler)
 
-	r.HandleFunc("/activate_serial/{table_name}/{serial_key}", handler.ActivateSerialsHandler)
-	r.HandleFunc("/reset_serial/{table_name}/{serial_key}", handler.ResetSerialsHandler)
-	r.HandleFunc("/about_serial/{table_name}/{serial_key}", handler.AboutSerialsHandler)
+	muxRouter.HandleFunc("/activate_serial/{table_name}/{serial_key}", handler.ActivateSerialsHandler)
+	muxRouter.HandleFunc("/reset_serial/{table_name}/{serial_key}", handler.ResetSerialsHandler)
+	muxRouter.HandleFunc("/about_serial/{table_name}/{serial_key}", handler.AboutSerialsHandler)
 
-	r.HandleFunc("/sendmail", handler.SendMailHandler)
+	muxRouter.HandleFunc("/sendmail", handler.SendMailHandler)
 
 	//s := http.StripPrefix("/static/asset_bundles/", http.FileServer(http.Dir("./static/asset_bundles/")))
-	//r.PathPrefix("/static/asset_bundles/").Handler(s).HandlerFunc(handler.ServeBundle);
-	r.HandleFunc("/static/media/{file}", handler.ServeStaticFiles)
+	//muxRouter.PathPrefix("/static/asset_bundles/").Handler(s).HandlerFunc(handler.ServeBundle);
+	//muxRouter.HandleFunc("/static/media/{file}", handler.ServeStaticFiles)
 
 	fmt.Printf("listening on %s...\n", port)
-	err := http.ListenAndServe(":"+port, r)
+
+	muxWithTimeout := http.TimeoutHandler(muxRouter, time.Second*20, "{is_activated:false}")
+	err := http.ListenAndServe(":"+port, muxWithTimeout)
 	if err != nil {
 		utils.PrintOutput(err.Error())
 	}
